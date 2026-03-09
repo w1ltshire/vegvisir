76e9b6fea9a394522efae76da88565641249fbee
wiltshire <wiltshire@ruf.st>
2026-03-09 19:33:36 +0200

    docs: change comments to rustdoc

--- vegvisir-controller/src/phy/uart.rs
+++ vegvisir-controller/src/phy/uart.rs
@@ -3,19 +3,17 @@
 /// # Example
 /// ```rust,no_run
 /// let mut config = Config::default();
-// 	config.baudrate = 9600;
-//
-// 	let uart: BufferedUart = {
-// 		buffered_uart!(
-//             p,
-//             p.USART1,
-//             config,
-//             PA10,
-//             PA9,
-//             1024,
-//             32
-//         )
-// 	};
+/// config.baudrate = 9600;
+///
+/// let uart: BufferedUart = buffered_uart!(
+///         p,
+///         p.USART1,
+///         config,
+///         PA10,
+///         PA9,
+///         1024,
+///         32
+/// );
 /// ```
 #[macro_export]
 macro_rules! buffered_uart {
