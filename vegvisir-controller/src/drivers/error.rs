a67f64cfd558b5ffe07e502684e20dbaf88b4ad1
wiltshire <wiltshire@ruf.st>
2026-03-13 19:57:25 +0200

    docs(drivers): add docs for `DriverError`

--- vegvisir-controller/src/drivers/error.rs
+++ vegvisir-controller/src/drivers/error.rs
@@ -1,13 +1,18 @@
 use thiserror::Error;
 
+/// Possible driver errors
 #[derive(Error, Debug)]
 pub enum DriverError {
+    /// Vec/String is too small
     #[error(transparent)]
     Capacity(#[from] heapless::CapacityError),
+    /// Failed to parse UTF-8
     #[error(transparent)]
     Utf8(#[from] core::str::Utf8Error),
+    /// UART error
     #[error(transparent)]
     Serial(#[from] embassy_stm32::usart::Error),
+    /// Failed to spawn a task
     #[error(transparent)]
     Spawn(#[from] embassy_executor::SpawnError),
 }
