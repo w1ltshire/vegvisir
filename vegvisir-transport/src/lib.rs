b1b44ab95a97db44fbfc2b0a735581c4d58e1b15
wiltshire <wiltshire@ruf.st>
2026-03-15 00:00:37 +0200

    feat(transport): add `Transport` trait

--- vegvisir-transport/src/lib.rs
+++ vegvisir-transport/src/lib.rs
@@ -1,3 +1,18 @@
 #![cfg_attr(not(doctest), doc = include_str!("../README.md"))]
 #![warn(missing_docs)]
 #![no_std]
+
+use core::error::Error;
+
+/// Transport trait for sending data to a peer
+#[allow(async_fn_in_trait)]
+pub trait Transport: Send + Sync {
+    /// The error type for this transport
+    type Error: Error;
+
+    /// Send data to the peer
+    async fn send(&mut self, data: &[u8]) -> Result<(), Self::Error>;
+
+    /// Receive data from the peer
+    async fn recv(&mut self, buf: &mut [u8]) -> Result<(), Self::Error>;
+}
