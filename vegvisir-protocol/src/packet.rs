b82a3bf366ece8a97f5702cfb279872feed11c71
wiltshire <wiltshire@ruf.st>
2026-03-14 11:36:35 +0200

    feat(protocol): `Packet::serialize`

--- vegvisir-protocol/src/packet.rs
+++ vegvisir-protocol/src/packet.rs
@@ -1,4 +1,6 @@
+use crc::{Crc, CRC_32_ISCSI};
 use defmt::Format;
+use heapless::Vec;
 use serde::{Deserialize, Serialize};
 
 /// Structure representing a single packet
@@ -8,23 +10,34 @@ pub struct Packet {
 	pub lora_address: u16,
 }
 
+impl Packet {
+	/// Serialize this packet with [`postcard`]
+	pub fn serialize(&self) -> Vec<u8, 255> {
+		let crc = Crc::<u32>::new(&CRC_32_ISCSI);
+		// 4 bytes = 32 bits of checksum
+		let size = postcard::serialize_with_flavor(self, postcard::ser_flavors::Size::default()).unwrap() + 4;
+		let mut buf: Vec<u8, 255> = Vec::new();
+		buf.resize(size, 0).unwrap();
+		postcard::to_slice_crc32(self, &mut buf, crc.digest()).unwrap();
+		buf
+	}
+}
+
 #[cfg(test)]
 mod tests {
 	extern crate std;
+	//extern crate alloc;
 
 	use crate::packet::Packet;
-	use crc::{Crc, CRC_32_ISCSI};
 	use std::println;
 
 	const EXAMPLE_PACKET: Packet = Packet {
-		lora_address: 0xF9F9
+		lora_address: 0xF9F9,
 	};
 
 	#[test]
 	fn serialize() {
-		let crc = Crc::<u32>::new(&CRC_32_ISCSI);
-		let mut buf = [0u8; 10];
-		let ser = postcard::to_slice_crc32(&EXAMPLE_PACKET, &mut buf, crc.digest()).unwrap();
+		let ser = EXAMPLE_PACKET.serialize();
 		println!("{ser:X?}");
 	}
 }
