a380ba5129bba00f355ce5b1e2fdf4a07d088f3e
wiltshire <wiltshire@ruf.st>
2026-03-15 17:11:21 +0200

    feat(transport): `Listener` trait

--- vegvisir-transport/src/lib.rs
+++ vegvisir-transport/src/lib.rs
@@ -16,3 +16,15 @@ pub trait Transport: Send + Sync {
     /// Receive data from the peer
     async fn recv(&mut self, buf: &mut [u8]) -> Result<(), Self::Error>;
 }
+
+/// Listener trait for accepting incoming connections (typically GCS side)
+#[allow(async_fn_in_trait)]
+pub trait Listener: Send + Sync {
+    /// The error type for this listener
+    type Error: Error;
+
+    /// Waits for the next incoming connection and returns a boxed `Transport`.
+    async fn accept(&self) -> Result<impl Transport, Self::Error>;
+    /// Close the listener
+    async fn close(self) -> Result<(), Self::Error>;
+}
