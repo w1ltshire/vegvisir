5045bcb8fd0ac6918d6700baf9d8bfb9e4891272
wiltshire <wiltshire@ruf.st>
2026-03-28 13:18:27 +0200

    feat: start rewriting gcs... controls......,,

---
+++ vegvisir-gcs-server/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    tracing::subscriber::set_global_default(
+        tracing_subscriber::FmtSubscriber::builder()
+            .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+            .finish(),
+    )
+        .unwrap();
+    tracing::info!("1");
+}
--- vegvisir-transport/src/tcp/mod.rs
+++
@@ -1,6 +0,0 @@
-extern crate std;
-
-/// Module providing TCP server functionality
-pub mod server;
-/// Module providing TCP client functionality
-pub mod client;
